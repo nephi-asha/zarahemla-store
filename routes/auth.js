8ce686054cf2dfe21ddb92db9c94a268df8eab80
nephi-asha <nephiasha2017@gmail.com>
2025-11-29 22:10:23 +0100

    refactor: improve logout handling by ensuring session destruction

--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,14 @@ router.get('/login', passport.authenticate('github'), (req, res) => {});
 
 router.get('/logout', function(req, res, next) {
     req.logout(function(err) {
-      if (err) { return next(err); }
-      res.redirect('/');
+        if (err) { return next(err); }
+        
+        req.session.destroy(function(err) {
+            if (err) { 
+              return next(err); 
+            }
+            res.redirect('/');
+        });
     });
 });
 
